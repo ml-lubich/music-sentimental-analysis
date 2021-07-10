a95511a5a56dd8dabc18aad336c81aa036be0d47
kingcobra2468 <eriktyryshkin@gmail.com>
2021-07-09 20:08:43 -0700

    added documentation throughout the repo

--- src/run.py
+++ src/run.py
@@ -4,12 +4,18 @@ from config import OUTPUT_DIR, START_YEAR, END_YEAR
 import pandas as pd
 import os.path
 
+# craete instances for the scrappers
 yt_link_scrapper = YoutubeResultsScrapper()
 wiki_scrapper = WikiHot100Scrapper(START_YEAR, END_YEAR)
 
+# get song metadata from wikipedia
 song_data = wiki_scrapper.scrape_all_tables()
 song_df = pd.DataFrame(song_data, columns=['year', 'artist', 'song'])
-song_df = song_df.set_index('year')  
+song_df = song_df.set_index('year')
 
-song_df['youtube_search_url'] = song_df.apply(yt_link_scrapper.get_first_result, axis=1)
+# get the urls for the songs
+song_df['youtube_search_url'] = song_df.apply(
+    yt_link_scrapper.get_first_result, axis=1)
+
+# save the whole dataset
 song_df.to_csv(os.path.join(OUTPUT_DIR, 'billboard_top_100.csv'))
